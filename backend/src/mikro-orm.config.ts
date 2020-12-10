422d1e9ee2a2d1356d58f753ab570a8405fddd83
Joshua Durai <joshua.durai@sap.com>
2020-12-10 09:05:10 -0800

    Initial commit

---
+++ backend/src/mikro-orm.config.ts
@@ -0,0 +1,15 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./common/constants";
+import { Post } from "./entities/Post";
+import path from 'path';
+
+export default {
+    migrations: {
+        path: path.join(__dirname, './migrations'), // path to the folder with migrations
+        pattern: /^[\w-]+\d+\.[tj]s$/
+    },
+    entities: [Post],
+    dbName: 'Jreddit',
+    type: 'postgresql',
+    debug: !__prod__
+} as Parameters<typeof MikroORM.init>[0];
---
+++ backend/src/resolvers/post.ts
@@ -0,0 +1,29 @@
+import { ORMContext } from "src/common/types";
+import { Post } from "../entities/Post";
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
+
+@Resolver()
+export class PostResolver {
+
+    @Query(() => [Post], {nullable: true})
+    getPosts(@Ctx() { em }: ORMContext): Promise<Post[]> {
+        return em.find(Post, {});
+    }
+
+   
+    @Query(() => Post, {nullable: true})
+    getPostById(@Ctx() { em }: ORMContext, @Arg('id') id: number): Promise<Post | null>{
+        return em.findOne(Post, {id});
+    }
+
+    @Mutation(() => Post)
+    async createPost(@Ctx() { em }: ORMContext, @Arg('title') title: string): Promise<Post> {
+        const post = await em.create(Post, {title});
+        try {
+            await em.persistAndFlush(post);
+            return post;
+        } catch(err) {
+            return err;
+        }
+    }
+}
